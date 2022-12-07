84c0e4d837cb012604c8afb68969ceb6d1f0b4ca
Bram Vanhoutte <bram@bothrs.com>
2022-12-07 18:50:26 +0100

    fix: add input limit

--- src/modules/upload-photo/components/message-input/MessageInput.tsx
+++ src/modules/upload-photo/components/message-input/MessageInput.tsx
@@ -2,10 +2,12 @@ import { StTextInput } from './MessageInput.styled'
 
 import type { TextInputProps } from 'react-native'
 
-export interface Props extends TextInputProps {
+export interface Props extends Omit<TextInputProps, 'maxLength'> {
   value: string
 }
 
 export const MessageInput = ({ ...props }: Props) => {
-  return <StTextInput {...props} placeholderTextColor="#f9f9f9" />
+  return (
+    <StTextInput {...props} placeholderTextColor="#f9f9f9" maxLength={30} />
+  )
 }
