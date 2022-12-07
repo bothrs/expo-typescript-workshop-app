b88260abc2284100f9b970775b7c5657cd5502c1
Bram Vanhoutte <bram@bothrs.com>
2022-12-07 09:46:51 +0100

    feat: compress image

--- src/modules/upload-photo/hooks/usePhotoUpload.ts
+++ src/modules/upload-photo/hooks/usePhotoUpload.ts
@@ -14,24 +14,39 @@ export const usePhotoUpload = () => {
       encoding: FileSystem.EncodingType.Base64,
     })
 
-    const imageBuffer = Buffer.from(imageBase64, 'base64')
+    const firsCharacterOfBase64 = imageBase64.charAt(0)
+
+    const fileType = firsCharacterOfBase64 === 'i' ? 'image/png' : 'image/jpg'
+
+    const compressedImage = await manipulateAsync(imageUri, [], {
+      compress: 0.8,
+      format: SaveFormat.JPEG,
+    })
+
+    const formData = new FormData()
+    formData.append('message', message)
+    formData.append('image', {
+      name: 'image.jpg',
+      type: fileType,
+      uri: compressedImage.uri,
+    } as unknown as Blob)
 
     try {
-      await axios.post(
-        'url',
-        { image: imageBuffer, message },
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-          onUploadProgress: (progressEvent) => {
-            const progress =
-              (progressEvent.loaded / (progressEvent.total || 0.01)) * 100
-            setUploadProgress(progress <= 100 && progress >= 0 ? progress : 0)
-          },
-        }
-      )
-    } catch {
+      await axios({
+        method: 'post',
+        url: 'https://odisee-workshop-server.vercel.app/api/upload',
+        data: formData,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        onUploadProgress: (progressEvent) => {
+          const progress =
+            (progressEvent.loaded / (progressEvent.total || 0.01)) * 100
+          setUploadProgress(progress <= 100 && progress >= 0 ? progress : 0)
+        },
+      })
+    } catch (error) {
+      console.log((error as AxiosError).message)
       console.log('Upload failed')
     }
 
